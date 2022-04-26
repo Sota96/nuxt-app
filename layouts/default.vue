405fb573cd628d13b043b2a1fd2d5a8077ab7597
Sota Tanino <s.tanino@hampstead.jp>
2022-04-26 16:29:20 +0900

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+ <div>
+ bbbbb
+    <Nuxt />
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data() {
+      return {
+          test: 'Hello'
+      }
+  }
+}
+</script>
---
+++ pages/test.vue
@@ -0,0 +1,14 @@
+<template>
+ <div>aaaa{{test}}</div>
+</template>
+
+<script>
+export default {
+  name: 'TestPage',
+  data() {
+      return {
+          test: 'Hello'
+      }
+  }
+}
+</script>
